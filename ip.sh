3e6fdea29bc47f52305ed2ffc41a22ce51763cfb
Gantai <50599491+mr-gantai@users.noreply.github.com>
2019-09-12 17:31:27 +0700

    Add files via upload

---
+++ ip.sh
@@ -0,0 +1,15 @@
+clear
+echo -e "\e[93m                                WELCOME TO CHECK IP BY GANTAI! \e[0m";
+sleep 1 
+echo -e "\e[36m                                CEK IP ADRESS MU DI BAGIAN INET \e[0m";
+echo -e "\e[96m======================÷================================================================\e[0m";
+echo -e 
+echo -e
+sleep 1
+ ifconfig
+echo -e
+echo -e 
+echo -e "\e[96m======================÷================================================================\e[0m";
+echo -e "\e[96m                                       TERIMAKASIH SAYANG \e[0m";
+echo -e 
+echo -e
